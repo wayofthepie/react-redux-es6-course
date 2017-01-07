ae93bfac398804b415b2ea0b086660335289f0a5
wayofthepie <wayofthepie@gmail.com>
2017-01-07 22:02:57 +0000

    Update how dispatching is defined

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -5,6 +5,7 @@
 
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
@@ -24,7 +25,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -49,7 +50,7 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes= {
-  dispatch: PropTypes.func,
+  actions: PropTypes.object.isRequired,
   courses: PropTypes.array.isRequired
 };
 
@@ -59,4 +60,10 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions,dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -12,4 +12,4 @@ export default function courseReducer(state = [], action) {
     default:
       return state;
   }
-};
+}
