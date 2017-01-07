d5e7cf833890e630b087c4109bdebdf2043a24dc
wayofthepie <wayofthepie@gmail.com>
2017-01-07 18:11:25 +0000

    Add courses page

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,13 @@
 'use strict';
 
 import React, {PropTypes} from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className='container-fluid'>
-        <p>Header here...</p>
+        <Header />
         {this.props.children}
       </div>
     );
