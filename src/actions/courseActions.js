db97f34ba227d9e9aa256b9e7e96ca5b1d17a428
wayofthepie <wayofthepie@gmail.com>
2017-01-07 22:06:12 +0000

    Moce action types to constants file

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -3,6 +3,8 @@
  */
 'use strict';
 
+import * as types from './actionTypes';
+
 export function createCourse(course) {
-  return {type: 'CREATE_COURSE', course}; // in es6, course is the same as course: course
+  return {type: types.CREATE_COURSE, course}; // in es6, course is the same as course: course
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,9 +3,11 @@
  */
 'use strict';
 
+import * as types from '../actions/actionTypes';
+
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
       return [...state, // es6 spread operator, puts values in state inline
         Object.assign({}, action.course) // deep copy of the course
       ];
