43aa484f03599b602c9015173261beb72f7decba
wayofthepie <wayofthepie@gmail.com>
2017-01-07 21:16:59 +0000

    Add course actions/reducer

---
+++ src/actions/courseActions.js
@@ -0,0 +1,8 @@
+/**
+ *
+ */
+'use strict';
+
+export function createCourse(course) {
+  return {type: 'CREATE_COURSE', course} // in es6, course is the same as course: course
+}
---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+'use strict';
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_COURSE':
+      return [...state, // es6 spread operator, puts values in state inline
+        Object.assign({}, action.course) // deep copy of the course
+      ];
+    default:
+      return state;
+  }
+}
