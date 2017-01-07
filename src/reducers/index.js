09d578fd6094212d817ca5d761860e6663525cc0
wayofthepie <wayofthepie@gmail.com>
2017-01-07 21:23:12 +0000

    Add store config

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+'use strict';
+import {combineReducers} from 'redux';
+import courses from './courseReducer';
+
+const rootReducer = combineReducers({
+  courses
+});
+
+export default rootReducer;
---
+++ src/store/configureStore.js
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+'use strict';
+
+import {createStore} from 'redux';
+import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+export default (initialState) => {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(reduxImmutableStateInvariant())
+  );
+}
