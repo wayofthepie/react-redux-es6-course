8c0be9df3442a3d043afba27d061dc815dbab84b
wayofthepie <wayofthepie@gmail.com>
2017-01-07 21:26:46 +0000

    Add provider for redux

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@
  */
 'use strict';
 
-import 'babel-polyfill'; // for some es6 feature transpiling, e.g Object.assign.
+import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
+import configureStore from './store/configureStore';
+import Provider from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
-import './styles/styles.css'; // webpack can import css!
+import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
