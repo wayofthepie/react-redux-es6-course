d870b6acee33f288fd3d601c53657ca6aca807d0
wayofthepie <wayofthepie@gmail.com>
2017-01-08 11:40:06 +0000

    Add authors actions

---
+++ src/actions/authorActions.js
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+'use strict';
+
+import AuthorApi from '../api/mockAuthorApi';
+import * as types from './actionTypes';
+
+export function loadAuthorsSuccess(authors) {
+  return {types: types.LOAD_AUTHORS_SUCCESS, authors};
+}
+
+export function loadAuthors() {
+  return dispatch => {
+    return AuthorApi.getAllAuthors().then(authors => {
+      dispatch(loadAuthorsSuccess(authors));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
