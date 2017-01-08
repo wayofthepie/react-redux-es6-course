96ab92c5842fba3b992f3797ca6610cc6f49b640
wayofthepie <wayofthepie@gmail.com>
2017-01-08 11:45:09 +0000

    Add authors reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,11 @@
 'use strict';
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
 const rootReducer = combineReducers({
-  courses
+  courses,
+  authors
 });
 
 export default rootReducer;
