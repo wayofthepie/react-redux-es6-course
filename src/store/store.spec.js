ac557627d6c9d5ceb06d6e81c01e638fb6887e4a
wayofthepie <wayofthepie@gmail.com>
2017-01-08 17:36:27 +0000

    Add test for store, actions and reducers

---
+++ src/store/store.spec.js
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+'use strict';
+
+import expect from 'expect';
+import {createStore} from 'redux';
+import rootReducer from '../reducers';
+import initialState from '../reducers/initialState';
+import * as courseActions from '../actions/courseActions';
+
+/**
+ * Integration test for actions, store and reducers.
+ */
+describe('Store', () => {
+  it('should handle creating courses', () => {
+    // arrange
+    const store = createStore(rootReducer, initialState);
+    const course = {
+      title: 'Clean Code'
+    };
+
+    // act
+    const action = courseActions.createCourseSuccess(course);
+    store.dispatch(action);
+
+    // assert
+    const actual = store.getState().courses[0];
+    const expected = {
+      title: 'Clean Code'
+    };
+    expect(actual, expected);
+  });
+});
