c954c4c52754e3cebf792b3529f718c5acd45089
wayofthepie <wayofthepie@gmail.com>
2017-01-08 10:44:06 +0000

    Add api calls as thunks

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -3,4 +3,4 @@
  */
 'use strict';
 
-export const CREATE_COURSE = 'CREATE_COURSE';
+export const LOAD_COURSES_SUCCESS = 'LOAD_COURSES_SUCCESS';
--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -4,7 +4,20 @@
 'use strict';
 
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return {type: types.CREATE_COURSE, course}; // in es6, course is the same as course: course
+
+export function loadCoursesSuccess(courses) {
+  return {type: types.LOAD_COURSES_SUCCESS, courses}; // in es6, course is the same as course: course
+}
+
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi.getAllCourses()
+      .then(courses => {
+        dispatch(loadCoursesSuccess(courses));
+      }).catch(error => {
+        throw(error);
+      });
+  };
 }
